1d190ab76176bbdac7bc1e83e9ccdeee3aa58737
phucnguyenvnn <118006368+phucnguyenvnn@users.noreply.github.com>
2022-11-12 00:08:00 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -15,7 +15,7 @@ ICACLS C:\Windows\installer /grant LittleFox:F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Username: LittleFox
-echo Password: 6454?foxy
+echo Username: administrator
+echo Password: 101567aA@
 echo You can login now!
 ping -n 10 127.0.0.1 >nul
