2d4e17a5d14873c68b38e2ffdad3f8547e22651e
phucnguyenvnn <118006368+phucnguyenvnn@users.noreply.github.com>
2022-11-12 00:29:10 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,14 +4,14 @@ net config server /srvcomment:"Windows Server By PhucNguyen" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
 net user PhucNguyen /add >nul
-net localgroup administrators LittleFox /add >nul
+net localgroup administrators PhucNguyen /add >nul
 net user PhucNguyen /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant PhucNguyen:F >nul
-ICACLS C:\Windows\installer /grant LittleFox:F >nul
+ICACLS C:\Windows\installer /grant PhucNguyen:F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
